8b76290a4ce2bdfd51c53d689b7d88a3f1ee7067
Elrey Belmonti <elbelmonti@gmail.com>
2018-06-04 12:14:20 -0700

    added a litte design

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
   $("#arabic_form").submit(function (e) {
+    $(".result").removeClass("d-none");
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     const userInput = $("#arabic_letters").val()
     const letters = userInput.split("");
     $(".result").empty();
+
     letters.forEach(function (letter) {
       const letterLowerCase = letter.toLowerCase();
       if (alphabet.includes(letterLowerCase)) {
         $(".result").append(`<img src=img/${letterLowerCase}.jpg alt="">`);
       } else {
-        $(".result").append(`<span class="space">${letterLowerCase}</span>`);
+        $(".result").append(`<div class="span d-inline">${letterLowerCase}</div>`);
       }
     });
     e.preventDefault();
