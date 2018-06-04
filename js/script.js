88031305bd68c9600bb28caad548118beac0a0d7
Elrey Belmonti <elbelmonti@gmail.com>
2018-06-04 09:06:10 -0700

    translater arabic letters to masonic images

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
+function arabicToMasonic(text) {
+  const letters = text.split("");
+  return letters;
+}
+
 $(document).ready(function () {
   $("#arabic_form").submit(function (e) {
     const userInput = $("#arabic_letters").val();
-    $(".result").text(userInput.toUpperCase());
+    console.log(arabicToMasonic(userInput));
+    arabicToMasonic(userInput).forEach(function (letter) {
+
+      $(".result").append(`<img src=img/${letter}.jpg alt="">`);
+    });
     e.preventDefault();
   });
 });
