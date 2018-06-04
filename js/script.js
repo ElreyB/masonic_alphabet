cf677f5a3473285acffeec4c116f88b03811559d
Elrey Belmonti <elbelmonti@gmail.com>
2018-06-04 10:01:51 -0700

    take in account for space and puncutaions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-function arabicToMasonic(text) {
-  const letters = text.split("");
-  return letters;
-}
-
 $(document).ready(function () {
   $("#arabic_form").submit(function (e) {
-    const userInput = $("#arabic_letters").val();
-    console.log(arabicToMasonic(userInput));
-    arabicToMasonic(userInput).forEach(function (letter) {
-
-      $(".result").append(`<img src=img/${letter}.jpg alt="">`);
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const userInput = $("#arabic_letters").val()
+    const letters = userInput.split("");
+    $(".result").empty();
+    letters.forEach(function (letter) {
+      const letterLowerCase = letter.toLowerCase();
+      if (alphabet.includes(letterLowerCase)) {
+        $(".result").append(`<img src=img/${letterLowerCase}.jpg alt="">`);
+      } else {
+        $(".result").append(`<span class="space">${letterLowerCase}</span>`);
+      }
     });
     e.preventDefault();
   });
